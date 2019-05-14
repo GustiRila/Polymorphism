52002d4c484b17f4f224952b40ae36f6e5914828
GustiRila <praja.rila31@gmail.com>
2019-05-14 14:43:12 +0700

    Animals

---
+++ Animals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Dog dog = new Dog();
+            animal = dog;
+            animal.Speak();
+
+            Cat cat = new Cat();
+            animal = cat;
+            animal.Speak();
+
+            Chicken chicken = new Chicken();
+            animal = chicken;
+            animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
